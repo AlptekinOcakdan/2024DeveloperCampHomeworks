5d385fdbdce8519bc86ed11d7db9092519089654
Alptekin <alptekin.ocakdan@gmail.com>
2024-02-05 19:49:37 +0300

    First Homework

---
+++ TrackMask/Business/Concrete/PttManager.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete;
+
+public class PttManager: ISupplierService
+{
+    private IApplicantService _applicantService;
+
+    public PttManager(IApplicantService applicantService)
+    {
+        _applicantService = applicantService;
+    }
+    public void GiveMask(Person person)
+    {
+        if (_applicantService.CheckPerson(person))
+        {
+            Console.WriteLine(person.FirstName + "'s mask given");
+        }
+        else
+        {
+            Console.WriteLine(person.FirstName + "'s mask NOT GIVEN");
+        }
+    }
+}
---
+++ TrackMask/WorkAround/Program.cs
@@ -0,0 +1,94 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+namespace WorkAround;
+
+internal static class Program
+{
+    static void Main(string[] args)
+    {
+        // Variables
+        Citizen citizen1 = new Citizen();
+
+        SayHello("Alptekin");
+        SayHello("Murat");
+        SayHello();
+        int result = SumUp(3, 5);
+
+        // Arrays
+
+        string[] students = new string[3];
+        students[0] = "Fatma";
+        students[1] = "Mahmut";
+        students[2] = "Özge";
+
+        for (int i = 0; i < students.Length; i++)
+        {
+            Console.WriteLine(students[i]);
+        }
+
+        string[] countries1 = new[] { "Ankara", "İstanbul", "İzmir" };
+        string[] countries2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+        countries2 = countries1;
+        countries1[0] = "Sakarya";
+        Console.WriteLine(countries2[0]);
+
+
+        foreach (string country in countries1)
+        {
+            Console.WriteLine(country);
+        }
+
+        List<string> newCountries1 = new List<string> { "Ankara", "İstanbul", "İzmir" };
+        newCountries1.Add("Ordu");
+
+        foreach (string country in newCountries1)
+        {
+            Console.WriteLine(country);
+        }
+
+        Person person1 = new Person();
+        person1.FirstName = "Alptekin";
+        person1.LastName = "Ocakdan";
+        person1.DateofBirthYear = 2000;
+        person1.NationalIdentity = 12345678910;
+
+
+        PttManager pttManager = new PttManager(new PersonManager());
+        pttManager.GiveMask(person1);
+    }
+
+    static void SayHello(string name = "nameless")
+    {
+        Console.WriteLine("Hello " + name);
+    }
+
+    static int SumUp(int num1, int num2)
+    {
+        int result = num1 + num2;
+        Console.WriteLine("Sum: " + result);
+        return result;
+    }
+
+    private static void Variables()
+    {
+        string message = "Hello";
+        double amount = 10000.5;
+        int number = 100;
+        bool isLoggedIn = false;
+
+        string name = "Alptekin";
+        string surname = "Ocakdan";
+        int birthYear = 2000;
+        long nationalityNumber = 12345678910;
+        Console.WriteLine(10000 * 1.08);
+    }
+}
+
+public class Citizen
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public int BirthYear { get; set; }
+    public long NationalityNumber { get; set; }
+}
